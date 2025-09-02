d6f251ca9cdbec954505b56638afa91cec511b69
Kajal Saini <sizukabts91@gmail.com>
2025-09-02 21:24:54 +0530

    Create navbar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navigation() {
+    
+  return (
+    <>
+    <div>
+      <nav>
+        <div className='Logo'>
+          <img src="" alt="Logo" />
+        </div>
+        <ul>
+          <li href='#'>Menu </li>
+          <li href='#'>Location </li>
+          <li href='#'>About </li>
+          <li href='#'>Contact Us </li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+    </>
+  )
+}
+
+export default Navigation
